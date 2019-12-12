be15cd0fd93cf1296a6e36f6314904f22083015c
mge19 <34735066+mge19@users.noreply.github.com>
2019-12-12 17:20:23 +0300

    Update problem18.cpp

--- problem18.cpp
+++ problem18.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
+using namespace std;
 int main()
 {
-	std::ifstream file("problem18.txt");
+	ifstream file("problem18.txt");
 	int result=0,**numbers=new int*[15];
 	for(int i=0;i<15;i++)
 	{
@@ -11,13 +12,13 @@ int main()
 			file>>numbers[i][j];
 			if(i>0 && j==0){numbers[i][j]+=numbers[i-1][j];}
 			else if(i>0 && j==i){numbers[i][j]+=numbers[i-1][j-1];}
-			else if(!(i==0 && j==0)){numbers[i][j]+=std::max(numbers[i-1][j-1],numbers[i-1][j]);}
-			if(i==14){result=std::max(numbers[i][j],result);}
+			else if(!(i==0 && j==0)){numbers[i][j]+=max(numbers[i-1][j-1],numbers[i-1][j]);}
+			if(i==14){result=max(numbers[i][j],result);}
 		}
 	}
 	delete[] numbers;
 	file.close();
-	std::cout<<result;
+	cout<<result;
 	getchar();
 	return 0;
 }
