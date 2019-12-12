256f413a3b67164825e818539f8c4512dca8b010
mge19 <34735066+mge19@users.noreply.github.com>
2019-12-12 19:56:41 +0300

    Update problem9.cpp

--- problem9.cpp
+++ problem9.cpp
@@ -1,12 +1,17 @@
 #include <bits/stdc++.h>
+using namespace std;
 int main()
 {
-	for(long int a=1;a<=333;a++)
+	for(long int a=1;a<=332;a++)
 	{
-		for(long int b=a;2*b+a<1000;b++)
+		for(long int b=a+1;2*b+a<1000;b++)
 		{
 			long int c=1000-a-b;
-			if(a*a+b*b==c*c){std::cout<<a*b*c;}
+			if(a*a+b*b==c*c)
+                        {
+                                cout<<a*b*c;
+                                break;
+                        }
 		}
 	}
 	getchar();
