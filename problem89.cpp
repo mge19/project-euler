d3b8e9fbd1230538fe162b02a68852c6eb17e6f6
mge19 <34735066+mge19@users.noreply.github.com>
2019-12-12 19:51:07 +0300

    Update problem89.cpp

--- problem89.cpp
+++ problem89.cpp
@@ -1,21 +1,22 @@
 #include <bits/stdc++.h>
+using namespace std;
 int main()
 {
 	int result=0;
-	std::ifstream file("p089_roman.txt");
+	ifstream file("p089_roman.txt");
 	while(!file.eof())
 	{
-		std::string str;
+		string str;
 		file>>str;
-		if(str.find("VIIII")!=std::string::npos){result+=3;}
-		else if(str.find("IIII")!=std::string::npos){result+=2;}
-		if(str.find("LXXXX")!=std::string::npos){result+=3;}
-		else if(str.find("XXXX")!=std::string::npos){result+=2;}
-		if(str.find("DCCCC")!=std::string::npos){result+=3;}
-		else if(str.find("CCCC")!=std::string::npos){result+=2;}
+		if(str.find("VIIII")!=string::npos){result+=3;}
+		else if(str.find("IIII")!=string::npos){result+=2;}
+		if(str.find("LXXXX")!=string::npos){result+=3;}
+		else if(str.find("XXXX")!=string::npos){result+=2;}
+		if(str.find("DCCCC")!=string::npos){result+=3;}
+		else if(str.find("CCCC")!=string::npos){result+=2;}
 	}
 	file.close();
-	std::cout<<result;
+	cout<<result;
 	getchar();
 	return 0;
 }
