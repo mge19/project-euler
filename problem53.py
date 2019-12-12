12b7ff784c794b8b5cf0242bd877f87cbbc919ba
mge19 <34735066+mge19@users.noreply.github.com>
2019-12-12 21:06:24 +0300

    Update problem53.py

--- problem53.py
+++ problem53.py
@@ -1,8 +1,8 @@
 fact=list()
 x=1
 for i in range(1,102):
-   fact.append(x)
-   x*=i
+    fact.append(x)
+    x*=i
 result=0
 i=0
 while(pow(2,i)<=1000000):
@@ -13,9 +13,9 @@ for j in range(i,101):
     else:
         x=int(j/2)
     while((fact[j]/(fact[j-x]*fact[x]))>1000000):
-       if(j%2!=0 or x!=int(j/2)):
-          result+=1
-       result+=1
-       x-=1
+        if(j%2!=0 or x!=int(j/2)):
+            result+=1
+        result+=1
+        x-=1
 print(result)
 x=input()
