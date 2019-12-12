89344315a51b81e789a8a467dd5327a6fd5af2c9
mge19 <34735066+mge19@users.noreply.github.com>
2019-12-12 20:22:29 +0300

    Update problem97.cpp

--- problem97.cpp
+++ problem97.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
+using namespace std;
 int main()
 {
 	long long int x=65536;
 	for(long int i=17;i<=7830457;i++){x=(2*x)%10000000000;}
-	std::cout<<(28433*x+1)%10000000000;
+	cout<<(28433*x+1)%10000000000;
 	getchar();
 	return 0;
 }
