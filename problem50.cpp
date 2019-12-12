4573be6e72dd25f81a0bbc74e430e8a435e5cc24
mge19 <34735066+mge19@users.noreply.github.com>
2019-12-12 18:04:26 +0300

    Update problem50.cpp

--- problem50.cpp
+++ problem50.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
+using namespace std;
 int main()
 {
 	long int result=0;
 	int max=0;
 	bool asal[1000000];
-	std::list<long int> asallar;
+	list<long int> asallar;
 	for(long int i=0;i<1000000;asal[i]=true,i++){}
 	for(long int i=2;i<1000000;i++)
 	{
@@ -14,11 +15,11 @@ int main()
 			for(long int k=2*i;k<=1000000;asal[k-1]=false,k+=i){}
 		}
 	}
-	for(std::list<long int>::iterator it=asallar.begin();it!=asallar.end();++it)
+	for(list<long int>::iterator it=asallar.begin();it!=asallar.end();++it)
 	{
 		long int temp;
 		int i;
-		std::list<long int>::iterator it1;		
+		list<long int>::iterator it1;		
 		for(i=1,it1=it,temp=0;temp<1000000;temp+=(*it1),++it1,++i)
 		{
 			if(asal[temp-1] && i>max)
@@ -28,7 +29,7 @@ int main()
 			}
 		}
 	}
-	std::cout<<result;
+	cout<<result;
 	getchar();
 	return 0;
 }
