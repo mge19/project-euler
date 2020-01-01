baac6d1aecd25f7680de9d622e655013e2f3b2a8
mge19 <34735066+mge19@users.noreply.github.com>
2020-01-01 19:12:10 +0300

    Update problem99.cpp

--- problem99.cpp
+++ problem99.cpp
@@ -14,12 +14,13 @@ int main()
 		unsigned long long int base,exp;
 		int i=0;
 		for(;str[i]!=',';i++){}
-		char* str2=new char[i],*str3=new char[str.length()-i-1];
+		char* str2=new char[i];
 		for(int x=0;x<i;x++){str2[x]=str[x];}
 		i++;
-		for(int j=0;i<str.length();i++,j++){str3[j]=str[i];}
 		base=atoi(str2);
-		exp=atoi(str3);
+		str2=new char[str.length()-i];
+		for(int j=0;i<str.length();i++,j++){str2[j]=str[i];}
+		exp=atoi(str2);
 		long double result=exp*log(base);
 		if(result>max)
 		{
