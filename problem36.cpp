53034d7b3b509e26c2c454579849c35606775281
mge19 <34735066+mge19@users.noreply.github.com>
2020-01-01 19:13:49 +0300

    Update problem36.cpp

--- problem36.cpp
+++ problem36.cpp
@@ -7,9 +7,8 @@ int main()
 	long int result=0;
 	for(long int i=1;i<1000000;i++)
 	{
-		char* str=new char[6],*strbinary=new char[20];
+		char* str=new char[20];
 		itoa(i,str,10);
-		itoa(i,strbinary,2);
 		bool correct=true;
 		for(int x=0;x<strlen(str)/2;x++)
 		{
@@ -21,9 +20,10 @@ int main()
 		}
 		if(correct)
 		{
-			for(int x=0;x<strlen(strbinary)/2;x++)
+			itoa(i,str,2);
+			for(int x=0;x<strlen(stry)/2;x++)
 			{
-				if(strbinary[x]!=strbinary[strlen(strbinary)-1-x])
+				if(str[x]!=strbinary[strlen(str)-1-x])
 				{
 					correct=false;
 					break;
@@ -32,7 +32,6 @@ int main()
 		}
 		if(correct){result+=i;}
 		delete str;
-		delete strbinary;
 	}
 	cout<<result;
 	getchar();
