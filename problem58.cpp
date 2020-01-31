051489a0ebe0378e114543eca05281bef3c8ab51
mge19 <34735066+mge19@users.noreply.github.com>
2020-01-31 20:12:00 +0300

    Add files via upload

---
+++ problem58.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+bool asal(long int number)
+{
+	int x=round(sqrt(number));
+	for(int i=2;i<=x;i++)
+	{
+		if(number%i==0){return false;}
+	}
+	return true;
+}
+int main()
+{
+	long int total=0;
+	long long int i=2;
+	for(;;i+=2)
+	{
+		total+=asal(i*i-i+1)+asal(i*i+i+1)+asal(i*i+1);
+		if(10*total<2*i){break;}
+	}
+	cout<<i-1;
+	getchar();
+	return 0;
+}
