d1a6b7683bfd1c7a61744bd931e55320f0e52a62
mge19 <34735066+mge19@users.noreply.github.com>
2020-02-03 16:44:20 +0300

    Update problem59.cpp

--- problem59.cpp
+++ problem59.cpp
@@ -1,26 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-void candidate_key(vector<char>& v1,vector<char>& v2,int i)
+void determine_keys(vector<char>& v1,vector<char>& v2,int i)
 {
-	for(char x='a';x<='z';x++)
+	if(i<3)
 	{
-		if((v1[i] xor x)>='a' && (v1[i] xor x)<='z'){v2.push_back(x);}
-		else if((v1[i] xor x)>='A' && (v1[i] xor x)<='Z'){v2.push_back(x);}
-		else if((v1[i] xor x)==' '){v2.push_back(x);}
-	}	
-}
-void eliminate_key(vector<char>& v1,vector<char>& v2,int i)
-{
-	for(int j=0;j<v2.size();j++)
-	{
-		if((v1[i] xor v2[j])<'a')
+		for(char x='a';x<='z';x++)
 		{
-			if((v1[i] xor v2[j])<'A' || (v1[i] xor v2[j])>'Z')
-			{
-				if((v1[i] xor v2[j])!=' '){v2.erase(v2.begin()+j);}
-			}
+			if((v1[i] xor x)>='a' && (v1[i] xor x)<='z'){v2.push_back(x);}
+			else if((v1[i] xor x)>='A' && (v1[i] xor x)<='Z'){v2.push_back(x);}
+			else if((v1[i] xor x)==' '){v2.push_back(x);}
 		}
-		else if((v1[i] xor v2[j])>'z'){v2.erase(v2.begin()+j);}
+	}	
+	else
+	{
+		for(int j=0;j<v2.size();j++)
+		{
+			if((v1[i] xor v2[j])<'a')
+			{
+				if((v1[i] xor v2[j])<'A' || (v1[i] xor v2[j])>'Z')
+				{
+					if((v1[i] xor v2[j])!=' '){v2.erase(v2.begin()+j);}
+				}
+			}
+			else if((v1[i] xor v2[j])>'z'){v2.erase(v2.begin()+j);}
+		}		
 	}
 }
 int main()
@@ -39,12 +42,12 @@ int main()
 		text.push_back(c);
 	}
 	file.close();
-	candidate_key(text,passcode1,0);
-	candidate_key(text,passcode2,1);
-	candidate_key(text,passcode3,2);
-	for(int i=3;passcode1.size()!=1;i+=3){eliminate_key(text,passcode1,i);}
-	for(int i=4;passcode2.size()!=1;i+=3){eliminate_key(text,passcode2,i);}
-	for(int i=5;passcode3.size()!=1;i+=3){eliminate_key(text,passcode3,i);}
+	for(int i=0;passcode1.size()!=1 || passcode2.size()!=1 || passcode3.size()!=1;i++)
+	{
+		if(i%3==0){determine_keys(text,passcode1,i);}
+		else if(i%3==1){determine_keys(text,passcode2,i);}
+		else{determine_keys(text,passcode3,i);}
+	}
 	cout<<"Passcode:"<<passcode1[0]<<passcode2[0]<<passcode3[0]<<"\nText:";
 	for(int i=0;i<text.size();i++)
 	{
