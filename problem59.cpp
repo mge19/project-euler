167049977ca0ba9d0f5b48b8571bac1a254bffa2
mge19 <34735066+mge19@users.noreply.github.com>
2020-02-03 16:08:51 +0300

    Update problem59.cpp

--- problem59.cpp
+++ problem59.cpp
@@ -1,5 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+void candidate_key(vector<char>& v1,vector<char>& v2,int i)
+{
+	for(char x='a';x<='z';x++)
+	{
+		if((v1[i] xor x)>='a' && (v1[i] xor x)<='z'){v2.push_back(x);}
+		else if((v1[i] xor x)>='A' && (v1[i] xor x)<='Z'){v2.push_back(x);}
+		else if((v1[i] xor x)==' '){v2.push_back(x);}
+	}	
+}
+void eliminate_key(vector<char>& v1,vector<char>& v2,int i)
+{
+	for(int j=0;j<v2.size();j++)
+	{
+		if((v1[i] xor v2[j])<'a')
+		{
+			if((v1[i] xor v2[j])<'A' || (v1[i] xor v2[j])>'Z')
+			{
+				if((v1[i] xor v2[j])!=' '){v2.erase(v2.begin()+j);}
+			}
+		}
+		else if((v1[i] xor v2[j])>'z'){v2.erase(v2.begin()+j);}
+	}
+}
 int main()
 {
 	ifstream file("p059_cipher.txt");
@@ -10,8 +33,7 @@ int main()
 		char x=file.get(),c=0;
 		while(x!=',' && !file.eof())
 		{
-			c*=10;
-			c+=(x-'0');
+			c=10*c+x-'0';
 			x=file.get();
 		}
 		text.push_back(c);
@@ -21,88 +43,25 @@ int main()
 	{
 		if(i%3==0 && passcode1.size()!=1)
 		{
-			if(i==0)
-			{
-				for(char x='a';x<='z';x++)
-				{
-					if((text[i] xor x)>='a' && (text[i] xor x)<='z'){passcode1.push_back(x);}
-					else if((text[i] xor x)>='A' && (text[i] xor x)<='Z'){passcode1.push_back(x);}
-					else if((text[i] xor x)==' '){passcode1.push_back(x);}
-				}
-			}
-			else
-			{
-				for(int j=0;j<passcode1.size();j++)
-				{
-					if((text[i] xor passcode1[j])<'a')
-					{
-						if((text[i] xor passcode1[j])<'A' || (text[i] xor passcode1[j])<'Z')
-						{
-							if((text[i] xor passcode1[j])!=' '){passcode1.erase(passcode1.begin()+j);}
-						}
-					}
-					else if((text[i] xor passcode1[j])>'z'){passcode1.erase(passcode1.begin()+j);}
-				}	
-			}
+			if(i==0){candidate_key(text,passcode1,i);}
+			else{eliminate_key(text,passcode1,i);}
 		}
 		else if(i%3==1 && passcode2.size()!=1)
 		{
-			if(i==1)
-			{
-				for(char x='a';x<='z';x++)
-				{
-					if((text[i] xor x)>='a' && (text[i] xor x)<='z'){passcode2.push_back(x);}
-					else if((text[i] xor x)>='A' && (text[i] xor x)<='Z'){passcode2.push_back(x);}
-					else if((text[i] xor x)==' '){passcode2.push_back(x);}	
-				}		
-			}
-			else
-			{
-				for(int j=0;j<passcode2.size();j++)
-				{
-					if((text[i] xor passcode2[j])<'a')
-					{
-						if((text[i] xor passcode2[j])<'A' || (text[i] xor passcode2[j])<'Z')
-						{
-							if((text[i] xor passcode2[j])!=' '){passcode2.erase(passcode2.begin()+j);}
-						}
-					}
-					else if((text[i] xor passcode2[j])>'z'){passcode2.erase(passcode2.begin()+j);}
-				}				
-			}			
+			if(i==1){candidate_key(text,passcode2,i);}
+			else{eliminate_key(text,passcode2,i);}			
 		}
 		else if(i%3==2 && passcode3.size()!=1)
 		{
-			if(i==2)
-			{
-				for(char x='a';x<='z';x++)
-				{
-					if((text[i] xor x)>='a' && (text[i] xor x)<='z'){passcode3.push_back(x);}
-					else if((text[i] xor x)>='A' && (text[i] xor x)<='Z'){passcode3.push_back(x);}
-					else if((text[i] xor x)==' '){passcode3.push_back(x);}
-				}				
-			}
-			else
-			{
-				for(int j=0;j<passcode3.size();j++)
-				{
-					if((text[i] xor passcode3[j])<'a')
-					{
-						if((text[i] xor passcode3[j])<'A' || (text[i] xor passcode3[j])<'Z')
-						{
-							if((text[i] xor passcode3[j])!=' '){passcode1.erase(passcode3.begin()+j);}
-						}
-					}
-					else if((text[i] xor passcode3[j])>'z'){passcode3.erase(passcode3.begin()+j);}
-				}				
-			}				
+			if(i==2){candidate_key(text,passcode3,i);}
+			else{eliminate_key(text,passcode3,i);}				
 		}
 	}
 	for(int i=0;i<text.size();i++)
 	{
 		if(i%3==0){result+=(text[i] xor passcode1[0]);}
 		else if(i%3==1){result+=(text[i] xor passcode2[0]);}
-		else{result+=(text[i] xor passcode3[0]);}
+		else{result+=(text[i] xor passcode3[0]);;}
 	}
 	cout<<result;
 	getchar();
