7cfa4eba32689f349cdd84a7872614e84823a74f
mge19 <34735066+mge19@users.noreply.github.com>
2020-02-21 09:30:00 +0300

    Update problem46.cpp

--- problem46.cpp
+++ problem46.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool asalmi(long int number)
+bool asalmi(int number)
 {
 	int x=round(sqrt(number));
 	for(int i=2;i<=x;i++)
@@ -11,14 +11,14 @@ bool asalmi(long int number)
 }
 int main()
 {
-	map<long int,bool> asal;
-	long int i;
+	map<int,bool> asal;
+	int i;
 	for(i=3;;i+=2)
 	{
 		if(!asalmi(i))
 		{
 			asal[i]=false;
-			long int j;
+			int j;
 			for(j=1;i>2*j*j && !asal[i-2*j*j];j++){}
 			if(i<=2*j*j){break;}
 		}		
