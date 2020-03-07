4daecb342f7d9f732c7cba02bde6ea4c977cc2b1
mge19 <34735066+mge19@users.noreply.github.com>
2020-03-07 23:21:13 +0300

    Update problem95.cpp

--- problem95.cpp
+++ problem95.cpp
@@ -3,33 +3,19 @@ using namespace std;
 int main()
 {
 	int maxloop=0;
-	long long int result;
-	vector<bool> visited(1000000,true);
-	for(long int i=2;i<=1000000;i++)
-	{
-		if(visited[i-1])
-		{
-			for(long int k=2*i;k<1000000;visited[k-1]=false,k+=i){}
-		}
-	}
+	long int result;
+	vector<long int> loop(1000000,1);
+	vector<bool> visited(1000000,false);
 	visited[0]=true;
 	for(long int i=2;i<=1000000;i++)
 	{
-		vector<long long int> numbers;
-		long long int number=i;
-		while(number<=1000000 && visited[number-1]==false)
+		vector<long int> numbers;
+		long int number=i;
+		while(visited[number-1]==false)
 		{
 			numbers.push_back(number);
 			visited[number-1]=true;
-			long long int a=1;
-			for(long long int x=number,j=2;x!=1;j++)
-			{
-				long long int u=j;
-				for(;x%j==0;u*=j,x/=j){}
-				a*=(u-1)/(j-1);
-			}
-			number=a-number;
-			cout<<i<<" "<<number<<endl;
+			number=loop[number-1];
 		}
 		for(int j=0;j<numbers.size();j++)
 		{
