6936f4d71d99f923512e9b079ff02c4a385e7f16
mge19 <34735066+mge19@users.noreply.github.com>
2020-03-14 22:37:33 +0300

    Update problem23.cpp

--- problem23.cpp
+++ problem23.cpp
@@ -2,20 +2,12 @@
 using namespace std;
 int main()
 {
-	int sum[28123];
+	vector<int> sum(28123,1);
 	bool can_be_written[28123];
 	long int result=0;
-	for(int i=1;i<=28123;i++)
+	for(int i=2;i<=28123;i++)
 	{
-		sum[i-1]=1;
-		can_be_written[i-1]=false;
-		for(int x=i,j=2;x!=1;j++)
-		{
-			int u=j;
-			for(;x%j==0;u*=j,x/=j){}
-			sum[i-1]*=(u-1)/(j-1);			
-		}
-		sum[i-1]-=i;
+		for(long int j=2*i;j<=28123;sum[j-1]+=i,j+=i){}
 	}
 	for(int i=1;i<=28123;i++)
 	{
