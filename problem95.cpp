1345c8bfbc96e2760dfa2a973dc608d493104c23
mge19 <34735066+mge19@users.noreply.github.com>
2020-03-14 22:31:54 +0300

    Update problem95.cpp

--- problem95.cpp
+++ problem95.cpp
@@ -9,20 +9,28 @@ int main()
 	visited[0]=true;
 	for(long int i=2;i<=1000000;i++)
 	{
+		if(loop[i-1]==1){visited[i-1]=true;}
+		for(long int j=2*i;j<=1000000;loop[j-1]+=i,j+=i){}
+	}
+	for(long int i=2;i<=1000000;i++)
+	{
 		vector<long int> numbers;
 		long int number=i;
-		while(visited[number-1]==false)
+		while(number<=1000000 && !visited[number-1])
 		{
 			numbers.push_back(number);
 			visited[number-1]=true;
 			number=loop[number-1];
 		}
-		for(int j=0;j<numbers.size();j++)
+		if(number<=1000000)
 		{
-			if(numbers[j]==number && numbers.size()-j>maxloop)
+			for(int j=0;j<numbers.size();j++)
 			{
-				maxloop=numbers.size()-j;
-				result=numbers[j];
+				if(numbers[j]==number && numbers.size()-j>maxloop)
+				{
+					maxloop=numbers.size()-j;
+					result=numbers[j];
+				}
 			}
 		}
 	}
