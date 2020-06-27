e37255bd242bf7a8c4db917280e7c8dac0ba282b
mge19 <34735066+mge19@users.noreply.github.com>
2020-06-27 22:07:59 +0300

    Update problem187.cpp

--- problem187.cpp
+++ problem187.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 int main()
 {
-	vector<bool> asal(100000000,true);
-	vector<long int> asallar,relprimes;
-	for(long int i=2;i<=100000000;i++)
+	long int result=0;
+	vector<int> relprime(100000000,0);
+	for(long int i=2;i<100000000;i++)
 	{
-		if(asal[i-1])
+		if(relprime[i-1]==0)
 		{
-			asallar.push_back(i);
-			long int x=asallar.size();
-			for(long int j=0;j<x && asallar[j]*asallar[x-1]<100000000;relprimes.push_back(asallar[j]*asallar[x-1]),j++){}
-			for(long int j=2*i;j<=100000000;asal[j-1]=false,j+=i){}
+			for(long int j=2*i;j<100000000;j+=i)
+			{
+				if(relprime[j/i-1]==0){relprime[j-1]=1;}
+				else{relprime[j-1]=2;}
+			}
 		}
+		else if(relprime[i-1]==1){result++;}
 	}
-	cout<<relprimes.size();
+	cout<<result;
 	getchar();
 	return 0;
 }
