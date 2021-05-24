239a7f84098967eb5838945750a51cb033aecb41
mge19 <34735066+mge19@users.noreply.github.com>
2021-05-24 18:19:53 +0300

    Update problem587.py

--- problem587.py
+++ problem587.py
@@ -1,8 +1,6 @@
 from math import atan,sqrt,pi
 n=1
-x=50.0
-while(x<=99.9):
+while(50*(1+((n+1-sqrt(2*n))*(n-1)/(n*n+1))-pi/4-atan((n-1)/sqrt(2*n))+atan((n-1)/(n+1)))/(1-pi/4)<=99.9):
     n+=1;
-    x=50*(1+((n+1-sqrt(2*n))*(n-1)/(n*n+1))-pi/4-atan((n-1)/sqrt(2*n))+atan((n-1)/(n+1)))/(1-pi/4)
-    print(str(n)+" "+str(x))
+print(n)
 x=input()
